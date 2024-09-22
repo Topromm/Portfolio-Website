ff15f2ef7f757af69d85c6f4604d646b1040b57e
Toprom <67751762+Topromm@users.noreply.github.com>
2024-09-22 20:44:32 +0300

    Modify styles

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function updateButton(isDarkMode) {
 function updateNavbarBackground(isDarkMode) {
     if (isDarkMode) {
         navbar.style.background = "linear-gradient(to right, #436674, #000000, #000000, #000000)";
-        document.querySelector(".hero-container").style.backgroundImage = "url(${darkModeBackground})";
+        document.querySelector(".hero-container").style.backgroundImage = `url(${darkModeBackground})`;
         document.getElementById("edge-blur").src = darkModeEdgeBlur;
         body.style.backgroundColor = "#929493";
     } else {
         navbar.style.background = "linear-gradient(to right, #ebb390, #677788, #677788, #677788)";
-        document.querySelector(".hero-container").style.backgroundImage = "url(${lightModeBackground})";
+        document.querySelector(".hero-container").style.backgroundImage = `url(${lightModeBackground})`;
         document.getElementById("edge-blur").src = lightModeEdgeBlur;
         body.style.backgroundColor = "#b7b9b8";
     }
@@ -41,12 +41,12 @@ updateButton(isDarkMode);
 if (isDarkMode) {
     body.classList.add("dark-mode");
     body.classList.remove("light-mode");
-    modeButton.innerHTML = '<img src="./images/moon.png" alt="Moon" class="icon" id="moon-icon">';
+    modeButton.innerHTML = `<img src="./images/moon.png" alt="Moon" class="icon" id="moon-icon">`;
     updateNavbarBackground(true);
 } else {
     body.classList.add("light-mode");
     body.classList.remove("dark-mode");
-    modeButton.innerHTML = '<img src="./images/sun.png" alt="Sun" class="icon" id="sun-icon">';
+    modeButton.innerHTML = `<img src="./images/sun.png" alt="Sun" class="icon" id="sun-icon">`;
     updateNavbarBackground(false);
 }
 
@@ -54,14 +54,14 @@ modeButton.addEventListener("click", () => {
     if (body.classList.contains("dark-mode")) {
         body.classList.add("light-mode");
         body.classList.remove("dark-mode");
-        modeButton.innerHTML = '<img src="./images/sun.png" alt="Sun" class="icon" id="sun-icon">';
+        modeButton.innerHTML = `<img src="./images/sun.png" alt="Sun" class="icon" id="sun-icon">`;
         localStorage.setItem("darkMode", "false");
         updateButton(false);
         updateNavbarBackground(false); // Change background image for light mode
     } else {
         body.classList.add("dark-mode");
         body.classList.remove("light-mode");
-        modeButton.innerHTML = '<img src="./images/moon.png" alt="Moon" class="icon" id="moon-icon">';
+        modeButton.innerHTML = `<img src="./images/moon.png" alt="Moon" class="icon" id="moon-icon">`;
         localStorage.setItem("darkMode", "true");
         updateButton(true);
         updateNavbarBackground(true); // Change background image for dark mode
